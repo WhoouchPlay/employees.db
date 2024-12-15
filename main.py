a177fce2514ed0c034967326ce65ce35c795f923
WhoouchPlay <stasolenchuk89@gmail.com>
2024-12-15 17:40:57 +0100

    hello

---
+++ main.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+
+
+app = Flask(__name__, static_folder="app/static", template_folder="app/templates")
+
+
+@app.get("/")
+def index():
+    return render_template("index.html", title="Головна сторінка")
+
+
+@app.get("/employees")
+def show_employees():
+    return render_template("employees.html", **context)
